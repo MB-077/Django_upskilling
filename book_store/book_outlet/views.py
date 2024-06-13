f037475165cff40583233b001d99d60d434b3732
Marul Bhardwaj <158758744+MB-077@users.noreply.github.com>
2024-06-13 14:38:25 +0530

    updated models in book_store

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+
+from .models import *
+from django.http import Http404
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'book_outlet/index.html', {'books': books})
+
+# def book_detail(request, id):
+def book_detail(request, slug):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404('Book not found')
+    # book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
+    return render(request, 'book_outlet/book_detail.html', {
+        'title': book.title,
+        'author': book.author,
+        'rating': book.rating,
+        'is_bestseller': book.is_bestseller,
+        })
